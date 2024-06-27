6a1a9cbb31869783fee9d343033ec02f4164b627
Wa <122730917+STKR2@users.noreply.github.com>
2024-06-27 15:44:14 +0300

    Update start.py

--- SessionGenerator/modules/start.py
+++ SessionGenerator/modules/start.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2023 WOODcraft
 from pyrogram import filters
 from pyrogram.types import Message
-
+from config import OWNER_ID
 from SessionGenerator import Opleech
 from SessionGenerator.utils import add_served_user, keyboard
 
@@ -10,7 +10,10 @@ from SessionGenerator.utils import add_served_user, keyboard
 @Opleech.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"🦋 Hey {message.from_user.first_name},\n\n⌘ Ini Bot {Opleech.mention},\nUntuk Membuat String Session Pyrogram Atau Telethon.\n\nJika Belum Punya API_ID & API_HASH, Silahkan Klik ❈ ʙᴜᴀᴛ ᴀᴘɪ ɪᴅ & ᴀᴘɪ ʜᴀꜱʜ ❈ ",
+        text=f"""-  تم تحديث بوت الاستخراج الجديد .
+- البوت يشتغل على كل سورسات البايروجرام  .
+
+- المطور الوحيد : [by developer](tg://user?id={OWNER_ID})""",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
