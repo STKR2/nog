b3fead2ebcd20a7b5a01282bd0f802d9403d0fd1
Wa <122730917+STKR2@users.noreply.github.com>
2024-06-27 15:51:40 +0300

    Update inline.py

--- SessionGenerator/utils/inline.py
+++ SessionGenerator/utils/inline.py
@@ -2,28 +2,30 @@
 # Copyright (c) 2023 WOODcraft
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from config import SUPPORT_CHAT
-
+from config import SUPPORT_CHAT, OWNER_ID
 
 keyboard = InlineKeyboardMarkup(
     [
         [InlineKeyboardButton(text="❈ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴛʀɪɴɢ ꜱᴇᴀꜱꜱɪᴏɴ ❈", callback_data="gensession")],
         [
-            InlineKeyboardButton(text="❈ ʙᴜᴀᴛ ᴀᴘɪ ɪᴅ & ᴀᴘɪ ʜᴀꜱʜ ❈", url="https://my.telegram.org/auth"),
+            InlineKeyboardButton(text="❈ ᴏᴡɴᴇʀ ❈", url=f"tg://user?id={OWNER_ID}"),
+            InlineKeyboardButton(text="❈ ᴄʜᴀɴɴᴇʟ ❈", url=SUPPORT_CHAT),
         ],
     ]
 )
+
 
 gen_key = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="❣️ 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐯1 ❣️", callback_data="pyrogram1"),
-            InlineKeyboardButton(text="🦋 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐯2 🦋", callback_data="pyrogram"),
+            InlineKeyboardButton(text="🦋 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐯2 🦋", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="🌼 𝐓𝐞𝐥𝐞𝐭𝐡𝐨𝐧 🌼", callback_data="telethon"),
         ],
-        [InlineKeyboardButton(text="🌼 𝐓𝐞𝐥𝐞𝐭𝐡𝐨𝐧 🌼", callback_data="telethon")],
+        [
+            InlineKeyboardButton(text="🔹 𝐎𝐰𝐧𝐞𝐫 🔹", url=f"tg://user?id={OWNER_ID}")
+        ]
     ]
-)
-
+    
 retry_key = InlineKeyboardMarkup(
     [[InlineKeyboardButton(text="⚡️ Coba Lagi ⚡️", callback_data="gensession")]]
 )
