fc39a497b3ea33cb795ea816ecda4ed417f31262
Wa <122730917+STKR2@users.noreply.github.com>
2024-06-27 16:21:03 +0300

    Update start.py

--- SessionGenerator/modules/start.py
+++ SessionGenerator/modules/start.py
@@ -2,7 +2,6 @@ from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from SessionGenerator import Opleech
 from SessionGenerator.utils import add_served_user, keyboard
-import config
 
 @Opleech.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
@@ -10,7 +9,7 @@ async def f_start(_, message: Message):
         text=f"""-  تم تحديث بوت الاستخراج الجديد .
 - البوت يشتغل على كل سورسات البايروجرام  .
 
-- المطور الوحيد : [by developer](tg://user?id={config.OWNER_ID})""",
+- المطور الوحيد : [by developer](https://t.me/RR8R9)""",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
